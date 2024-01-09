47fabd40d79c64dbe3ff15b94a523584edc0cd2e
arvirabago <rabago.arvilyzadessa@gmail.com>
2024-01-09 23:55:47 +0800

    JS for task 1 and 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,69 @@
 import { galleryItems } from './gallery-items.js';
+
+
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
+let lightboxInstance = null; 
+
+const galleryMarkup = galleryItems.map((image) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+      <img
+        class="gallery__image"
+        src="${image.preview}"
+        data-source="${image.original}"
+        alt="${image.description}"
+      />
+    </a>
+  </li>
+`).join('');
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const openModal = (source) => {
+  lightboxInstance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">
+  `);
+
+  lightboxInstance.show();
+
+  // Add event listener for the Escape key to close the modal
+  document.addEventListener('keydown', handleEscapeKey);
+};
+
+const handleEscapeKey = (e) => {
+  if (e.key === 'Escape') {
+    closeLightbox();
+  }
+};
+
+const closeLightbox = () => {
+ 
+  if (lightboxInstance && lightboxInstance.visible()) {
+    lightboxInstance.close();
+    lightboxInstance = null;
+
+ 
+    document.removeEventListener('keydown', handleEscapeKey);
+  }
+};
+
+const onContainerClick = (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains("gallery__image")) {
+    const source = e.target.dataset.source;
+    openModal(source);
+  }
+};
+
+galleryList.addEventListener("click", onContainerClick);
+
+
+document.addEventListener('click', (e) => {
+  if (lightboxInstance && !lightboxInstance.visible()) {
+    closeLightbox();
+  }
+});
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+document.addEventListener("DOMContentLoaded", function () {
+  const galleryList = document.querySelector('.gallery');
+
+  // Gallery items to ul.gallery
+  const galleryMarkup = galleryItems.map((image) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${image.original}">
+        <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+      </a>
+    </li>
+  `).join('');
+
+  galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+    // Initialize SimpleLightbox 
+    const lightbox = new SimpleLightbox('.gallery a');
+
+});
